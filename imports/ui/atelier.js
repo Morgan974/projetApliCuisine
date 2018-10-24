ae7df45b2977e12954622f49a44f8bca7b1d345a
Morgan974 <oconnormorgan974@gmil.com>
2018-10-24 14:46:36 +0400

    Etapa 4 - FIN

--- imports/ui/atelier.js
+++ imports/ui/atelier.js
@@ -6,4 +6,37 @@ Template.atelier.events({
     'click .delete'() {
         Collection.remove(this._id);
     },
+
+    /*
+    Permet l'affichage de données dans le modal
+    Les données sont extraite des cartes (voir atelier.html)
+    On les recupere et on les envoies dans le modal (Fin du body.html)    
+    */
+
+    'click .btn-edit'(event) {
+        const target = event.target;
+        const idAtelier = target.getAttribute("data-id");
+
+        const atelier = Collection.findOne({ _id: idAtelier });
+
+        const modalTitre = document.querySelector('#edt-titre');
+        const modalDescription = document.querySelector('#edt-description');
+        const modalDate = document.querySelector('#edt-date');
+        const modalHoraire = document.querySelector('#edt-horaire');
+        const modalDuree = document.querySelector('#edt-duree');
+        const modalPrix = document.querySelector('#edt-prix');
+        const modalImage = document.querySelector('#edt-image');
+        const modalNbrDispo = document.querySelector('#edt-nbrDispo');
+        const modalId = document.querySelector('#edt-id');
+
+        modalTitre.value = atelier.titre;
+        modalDescription.value = atelier.description;
+        modalDate.value = atelier.date;
+        modalHoraire.value = atelier.horaire;
+        modalDuree.value = atelier.duree;
+        modalPrix.value = atelier.prix;
+        modalImage.value = atelier.image;
+        modalNbrDispo.value = atelier.nbrDispo;
+        modalId.value = idAtelier;
+    },
 });
--- imports/ui/body.js
+++ imports/ui/body.js
@@ -12,3 +12,38 @@ Template.body.helpers({
         return Collection.find({});
     },
 });
+
+
+Template.body.events({
+    'submit .edition-atelier'(event) {
+        event.preventDefault();
+
+        const target = event.target;
+
+        const ModalEdtTitre = target.edtTitre.value;
+        const ModalEdtDescription = target.edtDescription.value;
+        const ModalEdtDate = target.edtDate.value;
+        const ModalEdtHoraire = target.edtHoraire.value;
+        const ModalEdtDuree = target.edtDuree.value;
+        const ModalEdtPrix = target.edtPrix.value;
+        const ModalEdtImage = target.edtImage.value;
+        const ModalEdtNbrDispo = target.edtNbrDispo.value;
+
+        const ModalId = target.edtId.value;
+
+        Collection.update(ModalId, {
+            $set: {
+                titre: ModalEdtTitre,
+                description: ModalEdtDescription,
+                date: ModalEdtDate,
+                horaire: ModalEdtHoraire,
+                duree: ModalEdtDuree,
+                prix: ModalEdtPrix,
+                image: ModalEdtImage,
+                nbrDispo: ModalEdtNbrDispo,
+            },
+        });
+
+        $('#Modal').modal('hide')
+    },
+});
